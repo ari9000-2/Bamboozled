d4154cfa97722cc1be7a1004cbac91df427d1895
Ariadne Si Suo <ari9000.suo@gmail.com>
2020-08-22 21:31:57 +0100

    Initial Commit

---
+++ teste/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  teste
+//
+//  Created by Si Suo on 2020/8/22.
+//  Copyright © 2020 Ariadne Si Suo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
